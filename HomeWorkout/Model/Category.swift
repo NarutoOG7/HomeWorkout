8118ebd2e2d695054b893073d690e2332ece2fef
Spencer Belton <23slowmojo@gmail.com>
2021-07-04 23:53:24 -0600

    Sections working :)

---
+++ HomeWorkout/Model/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Category.swift
+//  HomeWorkout
+//
+//  Created by Spencer Belton on 7/3/21.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+}
--- HomeWorkout/Services/DataService.swift
+++ HomeWorkout/Services/DataService.swift
@@ -11,19 +11,72 @@ class DataService {
     
     static let instance = DataService()
     
-    private let workouts = [
-        Workout(imageName: "pushup01", title: "Push-Up", duration: "30 sec"),
-        Workout(imageName: "situp01", title: "Sit-Ups", duration: "45 sec"),
-        Workout(imageName: "squat01", title: "Squats", duration: "60 sec")
+    private let categories = [
+        Category(title: "ARMS"),
+        Category(title: "BACK"),
+        Category(title: "CHEST"),
+        Category(title: "CORE"),
+        Category(title: "LEGS")
     ]
     
-    func getWorkouts() -> [Workout] {
-        return workouts
+    private let armWorkouts = [
+        Workout(imageName: "pushup01", title: "ARM", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "ARM", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "ARM", duration: "30 sec")
+    ]
+    private let backWorkouts = [
+        Workout(imageName: "pushup01", title: "BACK", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "BACK", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "BACK", duration: "30 sec")
+    ]
+    private let chestWorkouts = [
+        Workout(imageName: "pushup01", title: "CHEST", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "CHEST", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "CHEST", duration: "30 sec")
+    ]
+    private let coreWorkouts = [
+        Workout(imageName: "pushup01", title: "CORE", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "CORE", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "CORE", duration: "30 sec")
+    ]
+    private let legWorkouts = [
+        Workout(imageName: "pushup01", title: "LEG", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "LEG", duration: "30 sec"),
+        Workout(imageName: "pushup01", title: "LEG", duration: "30 sec")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+    func getArmWorkouts() -> [Workout] {
+        return armWorkouts
+    }
+    func getBackWorkouts() -> [Workout] {
+        return backWorkouts
+    }
+    func getChestWorkouts() -> [Workout] {
+        return chestWorkouts
+    }
+    func getCoreWorkouts() -> [Workout] {
+        return coreWorkouts
+    }
+    func getLegWorkouts() -> [Workout] {
+        return legWorkouts
     }
     
-  //  let stretches = [
-    
- //   ]
-    
-    
+    func getWorkouts(forCategory title: String) -> [Workout] {
+        switch title {
+        case "ARMS":
+            return getArmWorkouts()
+        case "BACK":
+            return getBackWorkouts()
+        case "CHEST":
+            return getChestWorkouts()
+        case "CORE":
+            return getCoreWorkouts()
+        case "LEGS":
+            return getLegWorkouts()
+        default: return getArmWorkouts()
+        }
+    }
 }
